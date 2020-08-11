1057b58bfb489e0ed6bab59cf52a098c9bbd2019
Bruno Pacheco <brunopacheco1@yahoo.com>
2020-08-12 01:07:12 +0200

    CBL keccak.cbl sample was changed to follow the coboldoc standard.

--- tests/resources/keccak.cbl
+++ tests/resources/keccak.cbl
@@ -1,24 +1,7 @@
        >>SOURCE FORMAT FREE
 *>**
-*>  The KECCAK module, that uses the Keccak-f[1600] permutation.
-*>
 *>  @author Laszlo Erdos - https://www.facebook.com/wortfee
 *>  @license LGPL-3.0
-*>
-*>  Date-Written: 2016-05-17
-*>  Fields in LINKAGE SECTION:
-*>    - LNK-KECCAK-RATE: The value of the rate r. The rate must be
-*>      a multiple of 8 bits in this implementation.           
-*>    - LNK-KECCAK-CAPACITY: The value of the capacity c. 
-*>      The rate and capacity must have r+c=1600.       
-*>    - LNK-KECCAK-INPUT: The input message.           
-*>    - LNK-KECCAK-INPUT-BYTE-LEN: The number of input bytes provided
-*>      in the input message.
-*>    - LNK-KECCAK-DELIMITED-SUFFIX: Bits that will be automatically
-*>      appended to the end of the input message, as in domain 
-*>      separation.
-*>    - LNK-KECCAK-OUTPUT: The buffer where to store the output.          
-*>    - LNK-KECCAK-OUTPUT-BYTE-LEN: The number of output bytes desired.
 *>
 *>  This library is free software; you can redistribute it and/or
 *>  modify it under the terms of the GNU Lesser General Public
@@ -34,6 +17,24 @@
 *>  License along with this library.
 *>**
 
+*>*
+*>  The KECCAK module, that uses the Keccak-f[1600] permutation.
+*>
+*>  Date-Written: 2016-05-17
+*>  Fields in LINKAGE SECTION:
+*>    - LNK-KECCAK-RATE: The value of the rate r. The rate must be
+*>      a multiple of 8 bits in this implementation.           
+*>    - LNK-KECCAK-CAPACITY: The value of the capacity c. 
+*>      The rate and capacity must have r+c=1600.       
+*>    - LNK-KECCAK-INPUT: The input message.           
+*>    - LNK-KECCAK-INPUT-BYTE-LEN: The number of input bytes provided
+*>      in the input message.
+*>    - LNK-KECCAK-DELIMITED-SUFFIX: Bits that will be automatically
+*>      appended to the end of the input message, as in domain 
+*>      separation.
+*>    - LNK-KECCAK-OUTPUT: The buffer where to store the output.          
+*>    - LNK-KECCAK-OUTPUT-BYTE-LEN: The number of output bytes desired.
+*>*
  IDENTIFICATION DIVISION.
  PROGRAM-ID. KECCAK.
 
@@ -188,9 +189,9 @@
  END PROGRAM KECCAK.
  
  
-*>******************************************************************************
+*>*
 *> Module that computes the Keccak-f[1600] permutation on the given state.
-*>******************************************************************************
+*>*
  IDENTIFICATION DIVISION.
  PROGRAM-ID. STATE-PERMUTE.
 
@@ -595,9 +596,9 @@
  END PROGRAM STATE-PERMUTE.
 
  
-*>******************************************************************************
+*>*
 *> Module to load a 64-bit value from STATE.
-*>******************************************************************************
+*>*
  IDENTIFICATION DIVISION.
  PROGRAM-ID. READ-LANE.
 
@@ -632,9 +633,9 @@
  END PROGRAM READ-LANE.
 
  
-*>******************************************************************************
+*>*
 *> Module to write a 64-bit value in STATE.
-*>******************************************************************************
+*>*
  IDENTIFICATION DIVISION.
  PROGRAM-ID. WRITE-LANE.
 
@@ -669,9 +670,9 @@
  END PROGRAM WRITE-LANE.
 
  
-*>******************************************************************************
+*>*
 *> Module to xor and write a 64-bit value in STATE.
-*>******************************************************************************
+*>*
  IDENTIFICATION DIVISION.
  PROGRAM-ID. XOR-LANE.
 
@@ -715,9 +716,9 @@
  END PROGRAM XOR-LANE.
  
  
-*>******************************************************************************
+*>*
 *> Module to rotate a 64-bit value.
-*>******************************************************************************
+*>*
  IDENTIFICATION DIVISION.
  PROGRAM-ID. ROL-LANE.
 
@@ -773,10 +774,10 @@
  END PROGRAM ROL-LANE.
 
  
-*>******************************************************************************
+*>*
 *> Module that computes the linear feedback shift register (LFSR) used to
 *> define the round constants (see [Keccak Reference, Section 1.2]).
-*>******************************************************************************
+*>*
  IDENTIFICATION DIVISION.
  PROGRAM-ID. LFSR86540.
 
